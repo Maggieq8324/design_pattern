0db9b16326c774895a6e662aa60bd5c49316d891
litong <lit@epsoft.com.cn>
2022-01-10 17:12:36 +0800

    test

--- src/com/coisini/design/principle/liskovsubstitution/methodout/Test.java
+++ src/com/coisini/design/principle/liskovsubstitution/methodout/Test.java
@@ -1,5 +1,11 @@
 package com.coisini.design.principle.liskovsubstitution.methodout;
 
+/**
+ * @Description
+ * @author Coisini
+ * @date
+ * @version 1.0
+ */
 public class Test {
 
     public static void main(String[] args) {
